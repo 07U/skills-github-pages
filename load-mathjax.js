01ab1eb780fdd5b043e74dccd5b09d7a9facedc5
Oz Davidi <43340507+07U@users.noreply.github.com>
2024-06-26 12:52:08 +0200

    Update load-mathjax.js

--- load-mathjax.js
+++ load-mathjax.js
@@ -37,7 +37,7 @@ MathJax = {
 
 (function () {
   var script = document.createElement('script');
-  script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js';
+  script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-chtml.js';
   script.async = true;
   document.head.appendChild(script);
 })();
