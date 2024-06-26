32bcdd601836303c8a445c0fb70a8a9685733a55
Oz Davidi <43340507+07U@users.noreply.github.com>
2024-06-26 12:54:41 +0200

    Update load-mathjax.js

--- load-mathjax.js
+++ load-mathjax.js
@@ -1,3 +1,10 @@
+(function () {
+  var script = document.createElement('script');
+  script.src = 'https://polyfill.io/v3/polyfill.min.js?features=es6';
+  script.async = true;
+  document.head.appendChild(script);
+})();
+
 MathJax = {
   loader: {load: ['[tex]/tagformat']},
   section: 1,
